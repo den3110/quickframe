560f0d8e7da195f77246a113dcd7db43764185a3
Admin <cedrusisme@gmail.com>
2024-08-10 00:15:32 +0700

    update

--- src/hooks/useRefreshToken.js
+++ src/hooks/useRefreshToken.js
@@ -1,18 +1,20 @@
 import { authApi } from "api";
 
 const useRefreshToken = async () => {
-  const refreshToken= localStorage.getItem('refreshToken');
+  const refreshToken = localStorage.getItem("refreshToken");
   if (!refreshToken) {
-    throw new Error('No refresh token found');
+    throw new Error("No refresh token found");
   }
 
   try {
-    const response = await authApi.refreshToken({ refreshToken });
-    localStorage.setItem('accessToken', response.data.accessToken);
-    localStorage.setItem('refreshToken', response.data.refreshToken);
+    const response = await authApi.refreshToken({
+      refresh_token: refreshToken,
+    });
+    localStorage.setItem("accessToken", response.data.access_token);
+    localStorage.setItem("refreshToken", response.data.refresh_token);
     return response.data.accessToken;
   } catch (error) {
-    console.error('Failed to refresh token:', error);
+    console.error("Failed to refresh token:", error);
     throw error;
   }
 };
